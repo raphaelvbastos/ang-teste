f5d338ce2c0e785b155b637ab4613e99be18bf2d
Raphael Bastos <raphael@dep.fem.unicamp.br>
2019-06-12 10:27:51 -0300

    inicio

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+ 
+app.use(express.static(`${__dirname}/dist/ang-teste`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/ang-teste/index.html`));
+});
+
+var porta = process.env.PORT || 8080;
+app.listen(porta);
