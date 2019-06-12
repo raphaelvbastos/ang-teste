ac07fa0825786c4522d877e3892d9737e201905b
Raphael Bastos <raphael@dep.fem.unicamp.br>
2019-06-12 10:43:31 -0300

    angular

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/ang-teste`));
+app.use(express.static(`${__dirname}/src/dist/ang-teste`));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/ang-teste/index.html`));
+res.sendFile(path.join(`${__dirname}/src/dist/ang-teste/index.html`));
 });
 
 var porta = process.env.PORT || 8080;
